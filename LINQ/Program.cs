5c45592f14f3b1891ac0b93fb7c6da3ac03c1d62
MohamadKhavaran <133672393+MohamadKhavaran@users.noreply.github.com>
2024-03-22 16:25:46 +0430

    First Experience

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,28 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            // LINQ Project in Visual Studio
+            int[] Arr = { 10, 10, 35, 60, 80, 15, 30, 20, 45, 90 };
+            var R1 = (from a in Arr select a).ToArray();
+            var R2 = (from a in Arr orderby a descending select a).ToArray();
+            var R3 = (from a in Arr orderby a ascending select a).ToArray();
+            var R4 = (from a in Arr where a > 40 select a).ToArray();
+            // Check All of Members And Return First Of This Element ; If Not Found That Get Error
+            var R5 = (from a in Arr where a == 20 select a).First();
+            // Check All of Members And Return First Of This Element ; If Not Found That Get 0
+            var R6 = (from a in Arr where a == 35 select a).FirstOrDefault();
+            // Dont Check All Members ;Navigate List to Get Element  And Return First Of This Element ; If Not Found That Get Error
+            var R7 = (from a in Arr where a == 90 select a).Single();
+            // Dont Check All Members ;Navigate List to Get Element  And Return First Of This Element ; If Not Found That Get 0
+            var R8 = (from a in Arr where a == 45 select a).SingleOrDefault();
+            var R9 = (from a in Arr select a).Count();
+            var R10 = (from a in Arr where a == 10 select a).Count();
+            //Check Exist Or Not Element
+            var R11 = (from a in Arr where a == 60 select a).Any();
+            var R12 = (from a in Arr select a).Max();
+            var R13 = (from a in Arr select a).Min();
+            var R14 = (from a in Arr select a).Sum();
+            Console.ReadKey();
         }
     }
 }
